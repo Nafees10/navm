d0305c10e842caa88061b8d0bd6a0091b17c1e53
Nafees10 <nafeeshassan16@gmail.com>
2019-10-29 01:40:15 +0500

    Finalized the VM
    100% its filled with bugs

--- source/navm/navm.d
+++ source/navm/navm.d
@@ -15,17 +15,20 @@ public import navm.defs : NaData;
 /// the VM (where the ~~magic~~executon happens)
 class NaVM{
 private:
-	void delegate()[][uinteger] _functions; /// instructions of functions loaded
-	NaData[][][uinteger] _functionsArguments; /// arguments of each functions' each instruction
-	void delegate()[] _currentFunction; /// instructions of function currently being executed
-	NaData[][] _currentArguments; /// arguments of instructions of function currently being executed
+	void delegate()[][] _functions; /// instructions of functions loaded
+	uinteger[] _functionsStackLength;
+	NaData[][][] _functionsArguments; /// arguments of each functions' each instruction
+
+	void delegate()[]* _currentFunction; /// instructions of function currently being executed
+	NaData[][]* _currentArguments; /// arguments of instructions of function currently being executed
 	
 	void delegate()* _instruction; /// pointer to next instruction
 	NaData[]* _arguments; /// pointer to next instruction's arguments
 	NaStack _stack; /// as the name says, stack
 	NaData _returnVal; /// return value of current function
+	bool keepRunning; /// set it to false to terminate execution
 	
-	ExternFunction[uinteger] _externFunctions; /// external functions 
+	ExternFunction[] _externFunctions; /// external functions 
 protected:
 	// instructions:
 
@@ -186,13 +189,13 @@ protected:
 		_stack.pop;
 	}
 	void jump(){
-		_instruction = &_currentFunction[(*_arguments)[0].intVal] - 1;
-		_arguments = &_currentArguments[(*_arguments)[0].intVal] - 1;
+		_instruction = &(*_currentFunction)[(*_arguments)[0].intVal] - 1;
+		_arguments = &(*_currentArguments)[(*_arguments)[0].intVal] - 1;
 	}
 	void jumpIf(){
 		if (_stack.pop.intVal == 1){
-			_instruction = &_currentFunction[(*_arguments)[0].intVal] - 1;
-			_arguments = &_currentArguments[(*_arguments)[0].intVal] - 1;
+			_instruction = &(*_currentFunction)[(*_arguments)[0].intVal] - 1;
+			_arguments = &(*_currentArguments)[(*_arguments)[0].intVal] - 1;
 		}
 	}
 
@@ -225,16 +228,134 @@ protected:
 	void doubleToInt(){
 		_stack.push(NaData(to!integer(_stack.pop.doubleVal)));
 	}
+
+	void returnVal(){
+		_returnVal = _stack.pop;
+	}
+	void terminate(){
+		keepRunning = false;
+	}
 public:
+	/// constructor
+	/// 
+	/// External Functions get added here
+	this(ExternFunction[] externalFunctions){
+		_externFunctions = externalFunctions.dup;
+	}
+	/// destructor
+	~this(){
 
+	}
+	/// Loads functions into VM, prepares them for execution
+	/// 
+	/// Returns: true if there was no error, false if there was and failed to load (probably because of an instruction not existing)
+	bool load(NaFunction[] functions){
+		void delegate()[Instruction] map = [
+			Instruction.ExecuteFunctionExternal : &executeExternalFunction,
+			Instruction.ExecuteFunction : &executeFunction,
 
+			Instruction.MathAddInt : &mathAddInt,
+			Instruction.MathSubtractInt : &mathSubtractInt,
+			Instruction.MathMultiplyInt : &mathMultiplyInt,
+			Instruction.MathDivideInt : &mathDivideInt,
+			Instruction.MathModInt : &mathModInt,
+
+			Instruction.MathAddDouble : &mathAddDouble,
+			Instruction.MathSubtractDouble : &mathSubtractDouble,
+			Instruction.MathMultiplyDouble : &mathMultiplyDouble,
+			Instruction.MathDivideDouble : &mathDivideDouble,
+			Instruction.MathModDouble : &mathModDouble,
+
+			Instruction.isSameInt : &isSameInt,
+			Instruction.isSameArrayInt : &isSameArrayInt,
+			Instruction.isLesserInt : &isLesserInt,
+			Instruction.isLesserSameInt : &isLesserSameInt,
+
+			Instruction.isSameDouble : &isSameDouble,
+			Instruction.isSameArrayDouble : &isSameArrayDouble,
+			Instruction.isLesserDouble : &isLesserDouble,
+			Instruction.isLesserSameDouble : &isLesserSameDouble,
+
+			Instruction.BinaryAnd : &binaryAnd,
+			Instruction.BinaryNot : &binaryNot,
+			Instruction.BinaryOr : &binaryOr,
+
+			Instruction.Push : &push,
+			Instruction.PushFrom : &pushFrom,
+			Instruction.PushRefFrom : &pushRefFrom,
+			Instruction.PushTo : &pushTo,
+			Instruction.Deref : &deref,
+			Instruction.Pop : &pop,
+			Instruction.Jump : &jump,
+			Instruction.JumpIf : &jumpIf,
+
+			Instruction.MakeArray : &makeArray,
+			Instruction.ReadElement : &readElement,
+			Instruction.ArrayLength : &arrayLength,
+			Instruction.ArrayLengthSet : &arrayLengthSet,
+			Instruction.Concatenate : &concatenate,
+			Instruction.Append : &append,
+
+			Instruction.IntToDouble : &intToDouble,
+			Instruction.DoubleToInt : &doubleToInt,
+
+			Instruction.ReturnVal : &returnVal,
+			Instruction.Terminate : &terminate,
+		];
+		_functions.length = functions.length;
+		_functionsArguments.length = functions.length;
+		_functionsStackLength.length = functions.length;
+		foreach(i, func; functions){
+			_functions[i].length = func.instructions.length;
+			_functionsArguments[i].length = func.arguments.length;
+			_functionsStackLength[i] = func.stackLength;
+			// make sure each instruction has args (arrays must match)
+			if (func.instructions.length != func.arguments.length)
+				return false;
+			foreach(index, instruction; func.instructions){
+				if (instruction ! in map)
+					return false;
+				_functions[i][index] = map[instruction];
+				_functionsArguments[i][index] = func.arguments[index].dup;
+			}
+		}
+		return true;
+	}
 
 
 	/// Calls a function
 	/// 
 	/// Returns: what that function returned
 	NaData execute(uinteger functionId, NaData[] arguments){
-
-		return _returnVal;
+		// save state of previous function
+		void delegate()[]* prevFunction = _currentFunction;
+		NaData[][]* prevFunctionArguments = _currentArguments;
+		void delegate()* prevInstruction = _instruction;
+		NaData[]* prevArguments = _arguments;
+		NaStack prevStack = _stack;
+		NaData prevReturnVal = _returnVal;
+		// prepare for this one
+		_stack = new NaStack(_functionsStackLength[functionId]);
+		_currentFunction = &_functions[functionId];
+		_currentArguments = &_functionsArguments[functionId];
+		_instruction = &(*_currentFunction)[0];
+		_arguments = &(*_currentArguments)[0];
+		_returnVal = NaData();
+		// start executing
+		while (keepRunning){
+			(*_instruction)();
+			_instruction++;
+			_arguments++;
+		}
+		NaData r = _returnVal;
+		// restore prev state
+		_currentFunction = prevFunction;
+		_currentArguments = prevFunctionArguments;
+		_instruction = prevInstruction;
+		_arguments = prevArguments;
+		_stack.destroy;
+		_stack = prevStack;
+		_returnVal = prevReturnVal;
+		return r;
 	}
 }
