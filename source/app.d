f093db8c4e9ee2aa2f2e9706dc6e9e1865aa2645
Nafees10 <nafeeshassan16@gmail.com>
2019-12-24 21:48:16 +0500

    Added functions to debug strVal and arrays in NaData

--- source/app.d
+++ source/app.d
@@ -4,8 +4,11 @@ import navm.navm;
 import utils.misc;
 import std.datetime.stopwatch;
 
+import core.memory : GC;
+
 
 void main(string[] args){
+	//GC.disable();
 	debug{
 		args = [args[0] , "sample"];
 	}
@@ -16,7 +19,21 @@ void main(string[] args){
 		write('\n');
 		return NaData();
 	}
-	NaVM vm = new NaVM([&writelnInt]);
+	NaData writelnIntArray(NaData[] args){
+		NaData[] array = args[0].arrayVal;
+		for (uinteger i = 0; i < array.length; i++){
+			writeln(array[i].intVal);
+		}
+		return NaData();
+	}
+	NaData writelnStr(NaData[] args){
+		foreach (arg; args){
+			write(arg.strVal);
+		}
+		write('\n');
+		return NaData();
+	}
+	NaVM vm = new NaVM([&writelnInt, &writelnStr, &writelnIntArray]);
 	vm.load(fileToArray(args[1]));
 	StopWatch sw;
 	sw.start;
