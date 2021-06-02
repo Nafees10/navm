abc755f71c3a8dff05269431d42e7f35f24734c4
Nafees10 <nafeeshassan16@gmail.com>
2021-06-02 11:44:21 +0500

    bug fixes in NaStack + added unittests

--- source/navm/navm.d
+++ source/navm/navm.d
@@ -21,6 +21,7 @@ public:
 	this(uinteger size = 1 << 16){
 		_stack.length = size;
 		_lastPtr = _stack.ptr + size;
+		_ptr = _stack.ptr;
 	}
 	~this(){
 		.destroy(_stack);
@@ -47,44 +48,74 @@ public:
 			_errored = true;
 			return;
 		}
-		_ptr[0 .. T.sizeof] = element;
+		_ptr[0 .. T.sizeof] = (cast(ubyte*)&element)[0 .. T.sizeof];
 		_ptr += T.sizeof;
 	}
 	/// pushes multiple elements to stack. last in array is first pushed
 	/// 
 	/// Either all are pushed, or none if there isn't enough space
-	void push(T)(T[] elements){
-		immutable static uinteger lenBytes = T.sizeof * elements.length;
+	void pushArray(T)(T[] elements){
+		immutable uinteger lenBytes = T.sizeof * elements.length;
 		if (_ptr + lenBytes > _lastPtr){
 			_errored = true;
 			return;
 		}
 		_ptr[0 .. lenBytes] = (cast(ubyte*)elements.ptr)[0 .. lenBytes];
+		_ptr += lenBytes;
 	}
 	/// pops from stack
 	/// 
 	/// Sets `val` to popped value
 	void pop(T)(ref T val){
-		immutable static uinteger lenBytes = T.sizeof;
-		if (_ptr < _stack.ptr + lenBytes){
+		if (_ptr < _stack.ptr + T.sizeof){
 			_errored = true;
 			return;
 		}
-		_ptr -= lenBytes;
+		_ptr -= T.sizeof;
 		val = *(cast(T*)_ptr);
 	}
 	/// pops multiple from stack. first popped is last in array
 	/// 
 	/// Writes popped values to `vals`. If there arent enough elements on stack, will not pop anything
-	void pop(T)(ref T[] vals){
-		immutable static uinteger lenBytes = T.sizeof * vals.length;
+	void popArray(T)(ref T[] vals){
+		immutable uinteger lenBytes = T.sizeof * vals.length;
 		if (_ptr < _stack.ptr + lenBytes){
 			_errored = true;
 			return;
 		}
 		_ptr -= lenBytes;
-		vals = (cast(T*)_ptr)[0 .. vals.length];
+		vals[0 .. vals.length] = (cast(T*)_ptr)[0 .. vals.length];
 	}
+}
+/// 
+unittest{
+	NaStack s = new NaStack();
+	s.push!integer(integer.max);
+	s.push!ubyte(255);
+	s.push!byte(127);
+	byte b;
+	ubyte ub;
+	integer[] iA = [0];
+	assert(s.errored == false);
+	s.pop(b);
+	s.pop(ub);
+	s.pop(iA[0]);
+	assert(b == 127);
+	assert(ub == 255);
+	assert(iA[0] == integer.max);
+	iA = [integer.max, integer.max >> 1, 0, 1025];
+	s.pushArray(iA);
+	iA[] = 0;
+	s.popArray(iA);
+	assert(iA == [integer.max, integer.max >> 1, 0, 1025]);
+	s.push(cast(integer)integer.max);
+	s.push(cast(integer)integer.max >> 1);
+	s.push(cast(integer)0);
+	s.push(cast(integer)1025);
+	iA[] = 0;
+	s.popArray(iA);
+	assert(iA == [integer.max, integer.max >> 1, 0, 1025]);
+	assert(s.errored == false);
 }
 
 /// NaVM
