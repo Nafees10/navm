ed0abcb3799d1b048e9b076fc85c7675135ecd8d
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-24 21:31:13 +0500

    No lines longer than 80

--- source/navm/bytecode.d
+++ source/navm/bytecode.d
@@ -247,7 +247,8 @@ unittest{
 	assert(" a b \"str\"".separateWhitespace == ["a", "b", "\"str\""]);
 	assert("a b 'c' \"str\"".separateWhitespace == ["a", "b", "'c'", "\"str\""]);
 	assert("\ta   \t b\"str\"".separateWhitespace == ["a", "b", "\"str\""]);
-	assert("   a   b  'c'\"str\"'c'".separateWhitespace == ["a", "b", "'c'", "\"str\"", "'c'"]);
+	assert("   a   b  'c'\"str\"'c'".separateWhitespace ==
+			["a", "b", "'c'", "\"str\"", "'c'"]);
 	assert("a 'b'#c".separateWhitespace == ["a", "'b'"]);
 	assert("a: a b#c".separateWhitespace == ["a:","a", "b"]);
 	assert("a 'b' #c".separateWhitespace == ["a", "'b'"]);
